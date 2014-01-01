ba687d9fd99ef9eafe5e646237511ed21f3d8f4f
erlaroche <elaroche@Athena.local>
2014-01-01 13:57:13 -0600

    Commit previous changes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,57 @@
-alert('hi');
+// var id = 'jofNR_WkoCE';
+// var genre = 'club';
+
+// var video = {
+//   youtubeId: id,
+//   genre: genre
+// }
+
+// console.log('Youtube ID:', video.youtubeId);
+
+// console.log('Video genre:', video.genre);
+
+// var dog = {
+//   name: 'Bill'
+// };
+
+// console.log("The dog's name is:", dog["name"]);
+
+// var book = {
+//   title: "How to Ruin People's Lives"
+// };
+
+// var propertyName = 'title';
+
+// console.log('The book title is:', book[propertyName]);
+
+
+// var reservations = {
+//   'Alice': true,
+//   'Charlie': true
+// };
+
+// var checkForReservation = function (name) {
+//   var r = reservations[name];
+//   // A double bang (!!) turns a truthy value into `true`,
+//   // and a falsey value into `false`
+//   return !!r;
+// };
+
+// console.log('Alice should have a reservation:', checkForReservation('Alice'));
+// console.log('Bob should not. Poor Bob', checkForReservation('Bob'));
+
+
+// var ringDoorbell = function () {
+//   console.log('Ding-dong!');
+// };
+// for (var i=0; i < 16; i++){
+//   ringDoorbell();
+// }
+
+var amplify = function (phrase, loudness) {
+  var result = '';
+  // TODO
+  return result;
+};
+
+console.log('Result should be "hello?!!!!!"', amplify('hello?', 5));

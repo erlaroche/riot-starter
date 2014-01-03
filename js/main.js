56988247b6df4da46d473da0b3926359a2a05060
erlaroche <elaroche@Athena.local>
2014-01-02 18:05:33 -0600

    Add previous aleterations

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 
 // TODO: (Step 2) Grab box template html
 // var boxTemplate = ???;
+var boxTemplate = $('#templates .box').html();
+
+
 
 $('#new-box').on('submit', function (e) {
   // Prevent the form from submitting
   // TODO: (Step 3) UNCOMMENT THIS LINE
-  // e.preventDefault();
+  e.preventDefault();
 
   // Grab the color that the user wants to create a box with
   var color = $('#new-box .color').val();
@@ -13,6 +16,11 @@ $('#new-box').on('submit', function (e) {
 
   // TODO: (Step 4) Use boxTemplate, color, and $.render to generate new html
   // var newBoxHtml = ???;
+
+
+  //this is where you left off
+  var newBoxHtml = $.render(boxTemplate);
+$('#color').append(newBoxHtml);
 
   console.log('Generated new box html:', newBoxHtml);
 

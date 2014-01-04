d95082de9b408d3a1a186d38c7bd471a425d5430
erlaroche <elaroche@Athena.local>
2014-01-04 12:17:06 -0600

    Add several correct Javascript problems from week 6 mon

--- js/main.js
+++ js/main.js
@@ -48,10 +48,100 @@
 //   ringDoorbell();
 // }
 
-var amplify = function (phrase, loudness) {
-  var result = '';
-  // TODO
-  return result;
+
+
+
+//create a function strMult that takes two parameters
+//str and times and returns that string multiplied by the given number of times
+// var strMult = function (str, times) {
+//   var result = '';
+//   for (;;) {
+//     if (times & 1) result += str;
+//     times >>= 1;
+//     if (times) str += str;
+//     else break;
+//   }
+//   return result;
+// };
+
+// console.log("Should be whatwhat:", strMult('what', 2));
+// console.log("Should be kkkkk:", strMult('k', 5));
+
+
+
+//complete the for loop such that the result is a combination of all
+//the strings in the array:
+//(is not working the way directions imply it should)
+// var array = ['apple', 'tangerine', 'grape', 'tangerine'];
+
+// var result = '';
+// for (var i=0; i < array.length; i++) {
+//   // ???
+//   console.log(array[i]);
+// };
+
+// console.log('Result should be "appletangerinegrapetangerine":', result);
+
+
+
+//complete the for loop such that the result is a combination 
+//of al the strings in the array, spearated by commas and a space, with an 
+//exclamation mark at the end:
+//unable to put spaces between outcomes
+// var array = ['bop it', 'pull it', 'twist it'];
+
+// var result = '';
+// for (var i = 0; i < array.length; i++) {
+//   console.log(array + "!")
+// };
+
+// console.log('Result should be "bop it, pull it, twist it!":', result);
+
+
+
+//write a line of code to trigger turkey-run
+// var poultryRace = $.observable({});
+
+// poultryRace.on('turkey-run', function () {
+//   alert('gobble! gobble!');
+// });
+
+// poultryRace.on('chicken-waddle', function () {
+//   alert('kachirp kachirp!');
+// });
+
+// poultryRace.trigger('turkey-run');
+
+
+
+
+//write the code so that the triggering
+//10-story-jump on stuntMan alerts the message
+// 'I beLIEVE!':
+// var stuntMan = $.observable({});
+
+// stuntMan.on('10-story-jump', function(){
+//   alert('I beLIEVE!');
+// })
+
+// stuntMan.trigger('10-story-jump')
+
+
+
+//write the missing line of code so that the following code makes it rain:
+//unable to make it rain
+var makeItRain = {
+  rainContent: 'dollar bills, yo'
 };
 
-console.log('Result should be "hello?!!!!!"', amplify('hello?', 5));
+console.log('Before:', makeItRain)
+
+var makeItRain = $.observable({});
+
+console.log('After:', makeItRain)
+
+makeItRain.on('rain', function () {
+  alert("It's raining " + makeItRain.rainContent + '.');
+});
+
+makeItRain.trigger('rain');
